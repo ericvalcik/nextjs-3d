7bab4214bc86b85fc3870078d9fa513a0c0e19ae
eric valcik <eric.valcik@gmail.com>
2022-10-16 02:40:28 +0200

    Add shadows

--- components/Model.jsx
+++ components/Model.jsx
@@ -1,11 +1,11 @@
-import React, {useMemo} from 'react'
+import React, {useLayoutEffect, useMemo} from 'react'
 import { useGLTF } from '@react-three/drei'
+import {applyProps} from "@react-three/fiber";
 
 export default function Model(props) {
-  const model = useGLTF("http://localhost:3000/scene.gltf");
-
-
-  return (
-    <primitive object={model.scene} />
-  )
+  const { scene, materials } = useGLTF("http://localhost:3000/scene.gltf");
+  useLayoutEffect(() => {
+    scene.traverse((obj) => obj.isMesh && (obj.receiveShadow = obj.castShadow = true))
+  })
+  return <primitive object={scene} {...props} />
 }
